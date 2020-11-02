4742aaf4fcb2e6108390a1b989b4faa60f11c611
im-faizz <70867378+im-faizz@users.noreply.github.com>
2020-11-02 15:18:58 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,124 @@
+const { create, decryptMedia } = require('@open-wa/wa-automate')
+const moment = require('moment')
+const urlShortener = require('./lib/shortener')
+const color = require('./lib/color')
+const { fetchMeme } = require('./lib/fetcher')
+const serverOption = {
+    headless: true,
+    qrRefreshS: 20,
+    qrTimeout: 0,
+    authTimeout: 0,
+    autoRefresh: true,
+    killProcessOnBrowserClose: true,
+    cacheEnabled: false,
+    chromiumArgs: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        // THIS MAY BREAK YOUR APP !!!ONLY FOR TESTING FOR NOW!!!
+        '--aggressive-cache-discard',
+        '--disable-cache',
+        '--disable-application-cache',
+        '--disable-offline-load-stale-cache',
+        '--disk-cache-size=0'
+    ]
+}
+
+const opsys = process.platform
+if (opsys === 'win32' || opsys === 'win64') {
+    serverOption.executablePath = 'usr/bin/google-chrome-stable'
+} else if (opsys === 'linux') {
+    serverOption.browserRevision = '737027'
+} else if (opsys === 'darwin') {
+    serverOption.executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+}
+
+const startServer = async () => {
+    create('Imperial', serverOption)
+        .then(client => {
+            console.log('[DEV] Red Emperor')
+            console.log('[SERVER] Server Started!')
+            // Force it to keep the current session
+            client.onStateChanged(state => {
+                console.log('[Client State]', state)
+                if (state === 'CONFLICT') client.forceRefocus()
+            })
+            // listening on message
+            client.onMessage((message) => {
+                msgHandler(client, message)
+            })
+            
+            
+            async function msgHandler (client, message) {
+    try {
+        const { type, id, from, t, sender, isGroupMsg, chat, caption, isMedia, mimetype, quotedMsg } = message
+        let { body } = message
+        const { name } = chat
+        let { pushname, verifiedName } = sender
+        // verifiedName is the name of someone who uses a business account
+        pushname = pushname || verifiedName
+        const prefix = '#'
+        body = (type == 'chat' && body.startsWith(prefix)) ? body : ((type == 'image' && caption) && caption.startsWith(prefix)) ? caption : ''
+        const command = body.slice(prefix.length).trim().split(/ +/).shift().toLowerCase()
+        const args = body.slice(prefix.length).trim().split(/ +/).slice(1)
+        const isCmd = body.startsWith(prefix)
+        const time = moment(t * 1000).format('DD/MM HH:mm:ss')
+        if (!isCmd && !isGroupMsg) return console.log('[RECV]', color(time, 'yellow'), 'Message from', color(pushname))
+        if (!isCmd && isGroupMsg) return console.log('[RECV]', color(time, 'yellow'), 'Message from', color(pushname), 'in', color(name))
+        if (isCmd && !isGroupMsg) console.log(color('[EXEC]'), color(time, 'yellow'), color(`${command} (${args.length})`), 'from', color(pushname))
+        if (isCmd && isGroupMsg) console.log(color('[EXEC]'), color(time, 'yellow'), color(`${command} (${args.length})`), 'from', color(pushname), 'in', color(name))
+        
+        
+        
+        // Checking function speed
+        // const timestamp = moment()
+        // const latensi = moment.duration(moment() - timestamp).asSeconds()
+        const isUrl = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi)
+        const uaOverride = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+        
+        
+        // cmd
+        switch (command) {
+        case 'tnc':
+        client.sendText(from, 'Bot ini adalah bot yg dibuat menggunakan java script.Menggunakan bot ini berarti anda menerima semua kebijakan kebijakan bot.data data anda tidak kami simpan seperti stiker dll.')
+        break
+        case 'help': {
+        const text = `Hi, ${pushname}! CMD ?stiker/sticker kirim foto dengan menggunakan ?sticker/stiker atau balas foto menggunakan ?sticker/stiker tersebut.Fitur baru coming soon`
+            client.sendText(from, text)
+            break
+          }
+        
+        case 'sticker':
+        case 'stiker':
+            if (isMedia) {
+                const mediaData = await decryptMedia(message, uaOverride)
+                const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
+                await client.sendImageAsSticker(from, imageBase64)
+            } else if (quotedMsg && quotedMsg.type == 'image') {
+                const mediaData = await decryptMedia(quotedMsg)
+                const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`
+                await client.sendImageAsSticker(from, imageBase64)
+            } else if (args.length == 1) {
+                const url = args[0]
+                if (!url.match(isUrl)) client.reply(from, 'Maaf, link yang kamu kirim tidak valid.', id)
+                await client.sendStickerfromUrl(from, url)
+                    .then((r) => {
+                        if (!r && r !== undefined) client.sendText(from, 'Maaf, link yang kamu kirim tidak memuat gambar.')
+                    })
+            } else {
+                client.reply(from, 'Tidak ada gambar! ', id)
+            }
+            break
+            
+            
+            default:
+            console.log(color('[ERROR]', 'red'), color(time, 'yellow'), 'Unregistered Command from', color(pushname))
+            break
+        }
+    } catch (err) {
+        console.log(color('[ERROR]', 'red'), err)
+    }
+}
+
+startServer()
+            
+            
